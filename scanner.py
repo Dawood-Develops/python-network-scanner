20487ffb2b748c4a8669ee792824d9c84eb85eec
Dawood Ahmed Saeedi <dahmadsaeedi@gmail.com>
2026-09-20 23:19:34 +0500

    Add multiple port scanning

--- scanner.py
+++ scanner.py
@@ -1,12 +1,12 @@
 import socket 
 def check_port(port):
-    s=socket.socket()
-    s.settimeout(2)
-    try:
-        s.connect(("www.example.com",port))
-        print("port",port, "opened")
-    except:
-        print("port",port, "not opened")
-
-check_port(80)
-check_port(443)
+        s=socket.socket()
+        s.settimeout(2)
+        try:
+            s.connect(("www.example.com",port))
+            print("port",port, "opened")
+        except:
+            print("port",port, "not opened")
+ports =[22,80,443,53]
+for port in ports:
+  check_port(port)
