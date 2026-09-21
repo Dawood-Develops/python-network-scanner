7751349f978504142812a1b46536af19a369d321
Dawood Ahmed Saeedi <dahmadsaeedi@gmail.com>
2026-09-22 02:01:45 +0500

    Improve port error handling

--- scanner.py
+++ scanner.py
@@ -1,12 +1,22 @@
 import socket 
+host_name=input("Enter the host name to check:")
+ip_address=socket.gethostbyname(host_name)
+print("IP address of", host_name, "is", ip_address)
+port_start=int(input("Enter the starting port number:"))
+port_end=int(input("Enter the ending port number:"))    
 def check_port(port):
         s=socket.socket()
         s.settimeout(2)
         try:
-            s.connect(("www.example.com",port))
+            s.connect((host_name,port))
             print("port",port, "opened")
-        except:
-            print("port",port, "not opened")
-ports =[22,80,443,53]
-for port in ports:
+        except socket.timeout:
+             print("port",port, "timeout")
+        except s.connectionRefusedError:
+             print("port",port, "closed")
+        
+            
+        
+        s.close()
+for port in range(port_start, port_end + 1):
   check_port(port)
