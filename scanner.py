096ea81cf04e4aba5cd08d947ee07cacb0bbc487
Dawood Ahmed Saeedi <dahmadsaeedi@gmail.com>
2026-09-23 00:33:51 +0500

    Add threaded scanning and result collection

--- scanner.py
+++ scanner.py
@@ -1,22 +1,37 @@
 import socket 
+import threading
 host_name=input("Enter the host name to check:")
 ip_address=socket.gethostbyname(host_name)
 print("IP address of", host_name, "is", ip_address)
 port_start=int(input("Enter the starting port number:"))
 port_end=int(input("Enter the ending port number:"))    
-def check_port(port):
+def check_port(host_name,port):#function that will take the host name and port and check the conncection
         s=socket.socket()
         s.settimeout(2)
         try:
             s.connect((host_name,port))
-            print("port",port, "opened")
+            return "OPEN"#wanted the output in the return form so the gui will work properly rather than just printing it on the terminal
         except socket.timeout:
-             print("port",port, "timeout")
-        except s.connectionRefusedError:
-             print("port",port, "closed")
-        
-            
-        
-        s.close()
-for port in range(port_start, port_end + 1):
-  check_port(port)
+             return "TIMEOUT"
+        except ConnectionRefusedError:
+             return "CLOSED"
+        except OSError:
+             return "ERROR"
+        finally:
+             s.close()
+results={}
+def scan_ports(host_name, port):
+     result = check_port(host_name, port)
+     results[port] = result
+     
+threads =[]
+for port in range(port_start, port_end + 1):#running loop from start port to end and print the result
+ thread = threading.Thread(target=scan_ports, args=(host_name,port))
+ threads.append(thread)
+ thread.start()
+
+
+for thread in threads:
+    thread.join()
+for port, result in results.items():
+    print("Port", port, ":", result)
